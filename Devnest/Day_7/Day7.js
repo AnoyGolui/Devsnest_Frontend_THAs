450aef40b88695421cda293d0643271defbbc956
AnoyGolui <67983302+AnoyGolui@users.noreply.github.com>
2021-07-15 20:59:17 +0530

    Update Day7.js

--- Devnest/Day_7/Day7.js
+++ Devnest/Day_7/Day7.js
@@ -1,4 +1,4 @@
-1. Write a JavaScript program to list the properties of a JavaScript object. Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; Sample Output: name,sclass,rollno
+//1. Write a JavaScript program to list the properties of a JavaScript object. Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; Sample Output: name,sclass,rollno
 let stud={   
   "name": "David Rayy",  
   "class" : "VI",   
@@ -9,12 +9,37 @@ for(let key in stud)
 
 console.log(stud);
 
-2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
+//2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
 var stud2=stud;
 delete stud2.rollno;
 console.log(stud2);
 
-3. Write a JavaScript program to get the length of a JavaScript object.  Sample object : var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
+//3. Write a JavaScript program to get the length of a JavaScript object.  Sample object : var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
 console.log(Object.keys(stud).length);
 
+//4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
+var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, 
+{ author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, 
+{ author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }];
 
+let prop2=[];
+for(let key in library)
+{ 
+console.log("book"+key);
+console.log(library[key].readingStatus);
+}
+
+5. Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes. Volume of a cylinder : V = πr2h where r is the radius and h is the height of the cylinder. Try To Attempt : Difficult Level Increased 
+const pi=3.14;
+let volu=function(r,h){
+return (r*r*h*pi);}
+console.log( "The volume is "+volu(3,5));
+
+6. Write a JavaScript program to sort an array of JavaScript objects.  Sample Object : var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }]; Expected Output: [[object Object] { author: "Walter Isaacson", libraryID: 4264, title: "Steve Jobs" }, [object Object] { author: "Suzanne Collins", libraryID: 3245, title: "Mockingjay: The Final Book of The Hunger Games" }, [object Object] { author: "The Road Ahead", libraryID: 1254, title: "Bill Gates" }]
+var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, 
+ { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, 
+ { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }]; 
+
+  library.sort(function (obj1, obj2) {
+    return obj1["libraryID"] - obj2["libraryID"];
+  });
